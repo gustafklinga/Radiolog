22615267111844a057c7a9ec0dc94a63407d6768
Gustaf Klinga <klingagustaf@gmail.com>
2025-04-18 09:38:50 +0200

    Small changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,9 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Välkommen till Radiolog.nu</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
+        Denna sida är under uppbyggnad och kommer att innehålla information och länkar som är relevanta för svenska radiologer.
       </p>
     </main>
   );
